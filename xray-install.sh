928e1f38ba9d3cce51f9bbdfd938cbc006fe499a
A learning person <ansonwan1988@gmail.com>
2021-04-04 17:20:49 +0800

    Update xray-install.sh

--- xray-install.sh
+++ xray-install.sh
@@ -659,7 +659,8 @@ install_geodata() {
       exit 1
     fi
   }
-  local download_link_geoip="https://github.com/v2fly/geoip/releases/latest/download/geoip.dat"
+  # local download_link_geoip="https://github.com/v2fly/geoip/releases/latest/download/geoip.dat"
+  local download_link_geoip="https://github.com/man2018/x-ray/raw/main/geoip/geoip.dat"
   local download_link_geosite="https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat"
   local file_ip='geoip.dat'
   local file_dlc='dlc.dat'
